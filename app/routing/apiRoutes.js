c5db569093b84c366a8d916c1ca1569f8ace2c39
jphamxuan <34908420+jphamxuan@users.noreply.github.com>
2018-04-20 23:25:31 -0700

    3rd commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,8 +3,44 @@ var path = require('path');
 var friends = require('../data/friends.js');
 
 module.exports = function(app) {
-	
-
+    
+//gets list of friends from data folder
 	app.get('/api/friends', function(req, res) {
 		res.json(friends);
+    });
+//adds new friend data from html
+    app.post('/api/friends', function(req, res) {
+        var userInput = req.body;
+
+        var userResponses = userInput.scores;
+
+        var matchName = '';
+		var matchImage = '';
+        var totalDifference = 10000;
+        
+//looks through friend data for best match
+        for (var i = 0; i < friends.length; i++) {
+
+			
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+
+			// If lowest difference, record the friend match
+			if (diff < totalDifference) {
+				// console.log('Closest match found = ' + diff);
+				// console.log('Friend name = ' + friends[i].name);
+				// console.log('Friend image = ' + friends[i].photo);
+
+				totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+			}
+		}
+        friends.push(userInput);
+
+		// Send appropriate response
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
 	});
+};
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './app/public')));
 
